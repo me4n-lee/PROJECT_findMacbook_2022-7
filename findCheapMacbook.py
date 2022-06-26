5ab5c0a71fbabbfec5e4aa6bde1e121a9209a387
keyleeu <minhk.lee21@gmail.com>
2022-06-26 09:53:08 +0900

    #3

--- findCheapMacbook.py
+++ findCheapMacbook.py
@@ -38,8 +38,8 @@ print(issue_title)
 print(issue_body)
 
 GITHUB_TOKEN = os.environ['GIT_TOKEN']
-REPO_NAME = "macbook"
-repo = Github(GITHUB_TOKEN).get_user().get_repo(REPO_NAME)
+REPO_NAME = "findmacbook"
+repo = Github(GIT_TOKEN).get_user().get_repo(REPO_NAME)
 if issue_body != '' and REPO_NAME == repo.name:
     res = repo.create_issue(title=issue_title, body=issue_body)
     print(res)
